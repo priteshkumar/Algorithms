86cbac33943d7c6d928fe6863cf84d7921f444a6
Pritesh Kumar <pritesh.kumar2@gmail.com>
2020-06-26 22:16:24 +0530

    add custom exception demo

--- src/com/mavixk/ds/exam/oop/TestException.java
+++ src/com/mavixk/ds/exam/oop/TestException.java
@@ -1,18 +1,44 @@
 package mavixk.ds.exam.oop;
 
-public class TestException {
-  public static void main(String[] args){
-    testException();
+class InvalidUserException extends RuntimeException {
+  private String message;
+
+  public InvalidUserException(String message){
+    this.message = message;
   }
 
-  public static void testException(){
-    try{
-      int a = 40/0;
+  @Override
+  public String toString() {
+    return "InvalidUserNameException" + ": " + this.message;
+  }
+}
+
+public class TestException {
+
+  public static void main(String[] args) {
+    testException();
+    testCustomException();
+  }
+
+  public static void testCustomException() {
+    try {
+      String name = "mavixk";
+      if (name.length() < 7) {
+        throw new InvalidUserException("username length < 8");
+      }
+    } catch (RuntimeException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void testException() {
+    try {
+      int a = 40 / 0;
       //return;//invalid since return in catch makes outer block unreachable
-    }catch(RuntimeException e){
+    } catch (RuntimeException e) {
       e.printStackTrace();
       return; //valid
-    }finally{
+    } finally {
       System.out.println("in final block");
       //return; //makes outer block unreachable
     }
